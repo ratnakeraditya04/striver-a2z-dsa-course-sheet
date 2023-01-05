556690a4eeef144aaf020b0ae0daff1443ae02a8
Aditya <ratnakeraditya04@gmail.com>
2023-01-05 18:01:02 +0530

    Added LeetCode DCC Folder

---
+++ LeetCode - Daily Coding Challenge - DCC - 05_01_2023/05_01_2023.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    static bool comp(vector<int> &a, vector<int> &b)
+    {
+        return a[1]<b[1];
+    }
+    int findMinArrowShots(vector<vector<int>>& points) {
+
+// My Approach - Taken Help from Discussion.
+// Optimised Solution - O(NlogN) - Time Complexity - Greedy Based Solution  
+        if(points.size() == 1)
+        {
+            return 1 ; 
+        }
+        sort(points.begin() , points.end(), comp) ; 
+        int cnt = 1 ; 
+        int end = points[0][1] ; 
+
+        for(int i = 1 ; i < points.size() ; i++)
+        {
+            if(points[i][0] <= end)
+            {
+                continue ;  
+            }
+            cnt++ ; 
+            end = points[i][1] ;   
+        }
+        return cnt ; 
+    }
+};

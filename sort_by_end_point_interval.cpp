61003078efc71dd44935ca18424267e5086f7a3e
Aditya <ratnakeraditya04@gmail.com>
2023-01-05 18:44:32 +0530

    Important Sorting Point

---
+++ sort_by_end_point_interval.cpp
@@ -0,0 +1,5 @@
+static bool cmp(vector<int>& a, vector<int>& b)
+{
+    return a[i]<(Ascending Order) or>(Descending Order) b[i];
+}
+// i is the index at which the intervals will be sorted based on that point.

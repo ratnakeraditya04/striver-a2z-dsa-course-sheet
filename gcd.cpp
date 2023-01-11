c2238e425200bfb9e4652bedc1a01db285029244
Aditya <ratnakeraditya04@gmail.com>
2023-01-12 00:03:18 +0530

    Added the code for finding gcd of two numbers

---
+++ gcd.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+
+int gcd(int a,int b)
+{
+	//Write your code here
+    
+    int result = 0 ; 
+    while(a > 0 && b > 0)
+    {
+        if(a > b ) a = a % b ; 
+        else b = b % a ; 
+    }
+    if(a == 0 ) return b ; 
+    return a ; 
+}
+

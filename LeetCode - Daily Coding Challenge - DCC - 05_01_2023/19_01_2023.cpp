77a6199c9b3d8a74223feabfc0d05044e9cdd53b
Aditya <ratnakeraditya04@gmail.com>
2023-01-19 22:16:52 +0530

    Added LeetCode DCC 19_01_2023

---
+++ LeetCode - Daily Coding Challenge - DCC - 05_01_2023/19_01_2023.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& A, int K) {
+                vector<int> count(K);
+        count[0] = 1;
+        int prefix = 0, res = 0;
+        for (int a : A) {
+            prefix = (prefix + a % K + K) % K;
+            res += count[prefix]++;
+        }
+        return res;
+    }
+};

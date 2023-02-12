44c9e226cf459c0bc57ee1b8b5d3abd588361dd7
Aditya <ratnakeraditya04@gmail.com>
2023-02-12 18:03:18 +0530

    Added Geeks for Geeks - POTD - 12_02_2023.cpp

---
+++ Geeks For Geeks - Problem of the Day/12_02_2023.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+class Node{
+public:
+    int val;
+    Node *next;
+    Node(int num){
+        val=num;
+        next=NULL;
+    }
+};
+
+class Solution{
+public:
+    bool isPrime(int x)
+    {
+        if(x == 2)
+        {
+            return true ; 
+        }
+        if(x == 3)
+        {
+            return true ;
+        }
+        for(int i = 2 ; i <= sqrt(x) + 1; i++)
+        {
+            if(x % i == 0)
+            {
+                return false ;
+            }
+        }
+        return true ;
+    }
+    Node *primeList(Node *head){
+        
+        if(head == NULL)
+        {
+            return head ; 
+        }
+        Node* temp = head ; 
+        while(temp != NULL)
+        {
+            if(temp->val == 1)
+            {
+                temp->val = 2 ; 
+                temp = temp->next ;
+            }
+            else if(isPrime(temp->val) == true )
+            {
+                temp = temp->next ;
+            }
+            else if(isPrime(temp->val) != true)
+            {
+                int x = temp->val ; 
+                int i = 1 ;
+                while(isPrime(temp->val) == false)
+                {
+                    if(isPrime(x - i)) temp->val = x - i ; 
+                    else if(isPrime(x + i)) temp->val = x + i ; 
+                    i++ ; 
+                }
+                temp = temp->next ; 
+            }
+        }
+        return head ;
+    }
+};

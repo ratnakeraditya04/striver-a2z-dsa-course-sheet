e8a6105266b0dd35b3baef77448b741ee18d1d11
Aditya <ratnakeraditya04@gmail.com>
2023-02-18 13:03:36 +0530

    Added LeetCode DCC 18_02_2023

---
+++ LeetCode - Daily Coding Challenge - DCC - 05_01_2023/18_02_2023.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {} 
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void invert(TreeNode* root){
+        if(root == NULL)
+            return;
+        
+        TreeNode* temp = NULL;
+        temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        
+        invert(root->left);
+        invert(root->right);
+        
+        return;
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        invert(root);
+        return root;
+    }
+};

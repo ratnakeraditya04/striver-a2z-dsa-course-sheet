efe00bfc1b26eb9545f6e77655db5bb271697979
Aditya <ratnakeraditya04@gmail.com>
2023-03-13 01:16:18 +0530

    Started Algorithms Badge = 12_03_2023 and solved some more Problems.

---
+++ Algorithms-Badge-1-12_03_2023/35.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        // Approach - Binary Search - O(logN) - Time Complexity
+        //         int low = 0 ;
+        //         int high = nums.size() - 1 ;
+        //         while(low <= high)
+        //         {
+        //             int mid = low + (high - low)/2 ;
+        //             if(nums[mid] == target)
+        //             {
+        //                 return mid ;
+        //             }
+        //             else if(nums[mid] > target)
+        //             {
+        //                 high = mid - 1 ;
+        //             }
+        //             else if(nums[mid] < target)
+        //             {
+        //                 low = mid + 1 ;
+        //             }
+        //         }
+        //         return low ;
+
+        // int low = 0 ;
+        // int high = nums.size() - 1 ;
+        // while(low <= high)
+        // {
+        //     int mid = low + (high - low)/ 2 ;
+
+        //     if(nums[mid] == target)
+        //     {
+        //         return mid ;
+        //     }
+        //     else if(nums[mid] > target)
+        //     {
+        //         high = mid - 1 ;
+        //     }
+        //     else
+        //     {
+        //         low = mid + 1 ;
+        //     }
+        // }
+        // return low ;
+
+        // Attempted Again - O(logN) - Time Complexity
+
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
